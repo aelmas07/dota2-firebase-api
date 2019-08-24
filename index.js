07971cc58a17ef794b78caaa3afdb3c1b3ea56e8
Onur Uzun <onur.uzunn@hotmail.com>
2019-08-24 16:42:31 +0300

    files added

---
+++ index.js
@@ -0,0 +1,43 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account-key.json');
+const fs = require('fs')
+
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+function postData() {
+
+    //firestore post
+    const jsonFile = fs.readFileSync('./heroes.json')
+    const heroes = JSON.parse(jsonFile);
+
+    return db.collection('dota').doc('heroes')
+        .set(heroes).then(() => {
+            console.log("Fresh Meat!!");
+        });
+};
+
+postData();
+
+function getDataFirestore() {
+
+    try {
+
+        //firestore get document
+        const docRef = db.doc("dota/heroes");
+
+        docRef.get().then((data) => {
+            if (data && data.exists) {
+                const responseData = data.data();
+                console.log(JSON.stringify(responseData, null, "  "));
+            }
+        })
+    } catch (error) {}
+
+}
+
+getDataFirestore();
